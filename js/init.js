ce5e21fddd0528e04e1bdd3d1171eb9e258371b5
nataliarosas2016 <150367908+nataliarosas2016@users.noreply.github.com>
2023-11-09 10:11:09 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -2,11 +2,11 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-  apiKey: "AIzaSyCrHoVHPsrYHQ8HKah5UEttbQqHydegK0k",
-  authDomain: "autenfb2023-5d562.firebaseapp.com",
-  projectId: "autenfb2023-5d562",
-  storageBucket: "autenfb2023-5d562.appspot.com",
-  messagingSenderId: "1089728085495",
-  appId: "1:1089728085495:web:ec62b3c83d3d98f7ac42f2",
-  measurementId: "G-S1T5SCE6JP"
+    apiKey: "AIzaSyAl5XV2oxhKOTVDHihNlCd98o0iaX8rPJQ",
+    authDomain: "aut43nrm.firebaseapp.com",
+    projectId: "aut43nrm",
+    storageBucket: "aut43nrm.appspot.com",
+    messagingSenderId: "290666856568",
+    appId: "1:290666856568:web:5102b0c2caafe69a606d8d",
+    measurementId: "G-P07Z0DTFRL"
     });
